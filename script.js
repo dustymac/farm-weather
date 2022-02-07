baaed4ba279668544836cb955f8c224f09ad3911
Dusty McWilliams <dustymac@users.noreply.github.com>
2022-02-06 22:55:30 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let weather =  {
+    "apiKey": "a9c25e0052e0b3b13adbfb60962a1228",
+        fetchWeather: function(city) {
+            fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=imperial&APPID="
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data ) => this.displayWeather(data));
+        },
+        displayWeather: function(data) {
+            const { name } = data;
+            const { icon, description } = data.weather[0];
+            const { temp, humidity } = data.main;
+            const { speed } = data.wind;
+            document.querySelector(".city").innerText = "Weather in " + name;
+            document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +".png";
+            document.querySelector(".description").innerText = description;
+            document.querySelector(".temp").innerText = Math.floor(temp) + "°F";
+            document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+            document.querySelector(".wind").innerText = "Wind: " + Math.floor(speed) + "mph";
+            document.querySelector(".weather.loading").style.display = "block";
+        },
+        search: function() {
+            this.fetchWeather(document.querySelector(".search-bar").value);
+        }
+    
+};
+
+document.querySelector(".search button").addEventListener("click", function() {
+        weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
+
